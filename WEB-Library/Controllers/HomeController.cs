0421054d859aa113d684c88881d12830a8d1bdc5
Olga <olgamaluta44@gmail.com>
2024-03-04 18:18:54 +0200

    Update Index and add MVC logic

--- WEB-Library/Controllers/HomeController.cs
+++ WEB-Library/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WEB_Library.Models;
 
 namespace WEB_Library.Controllers
 {
@@ -10,8 +11,23 @@ namespace WEB_Library.Controllers
     {
         public ActionResult Index()
         {
+            
             return View();
         }
+        public ActionResult AdminPage()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
 
         public ActionResult About()
         {
@@ -25,6 +41,15 @@ namespace WEB_Library.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+    }
+
+    public class BookController : Controller
+    {
+        public ActionResult GetBook()
+        {
+            var book = new Book { Title = "ASP.NET MVC Guide" };
+            return Json(new { title = book.Title }, JsonRequestBehavior.AllowGet);
         }
     }
 }
