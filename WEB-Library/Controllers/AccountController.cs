f22d37cf103382b66aa794be95c96d51db1e590e
Olga <olgamaluta44@gmail.com>
2024-04-09 19:10:18 +0300

    Registration Logic

--- WEB-Library/Controllers/AccountController.cs
+++ WEB-Library/Controllers/AccountController.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WEB_Library.Models;
 
 namespace WEB_Library.Controllers
 {
-    public class AccountController : Controller
-    {
-        [HttpPost]
-        public ActionResult Login(string first, string password)
+    
+    
+        public class AccountController : Controller
         {
-            // Проверка логина и пароля, обработка входа
-            return RedirectToAction("Index", "Home"); // Перенаправление после успешного входа
+            private AccountContext db = new AccountContext();
+
+            // GET: Account/Register
+            [HttpGet]
+            public ActionResult Registr()
+            {
+                return View();
+            }
+
+            public ActionResult Login()
+            {
+                return View();
+            }
+
+            // POST: Account/Register
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Registr(RegisterViewModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    var account = new Account { Username = model.Email, Password = model.Password };
+                    db.Account.Add(account);
+                    db.SaveChanges();
+
+                    // Optionally, log the user in after registration
+                    // FormsAuthentication.SetAuthCookie(user.Username, false);
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                return View(model);
+            }
         }
     }
-}
---
+++ WEB-Library/Models/AccountContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WEB_Library.Models
+{
+    public class AccountContext : DbContext
+    {
+        public DbSet<Account> Account{ get; set; }
+    }
+
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
